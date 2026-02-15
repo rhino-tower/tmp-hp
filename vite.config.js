ed28dd17ea7618f3e7244dc8adf6fb1199ff297c
rhino-tower <yukis.a0516@icloud.com>
2026-02-15 19:14:27 +0900

    add: deploy workflow

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    // GitHub Pagesにデプロイする場合、リポジトリ名をbaseに設定します。
+    // 例: https://username.github.io/my-repo/ なら base: '/my-repo/'
+    // 現在は相対パス設定にしていますが、うまくいかない場合はリポジトリ名を設定してください。
+    base: './',
+})
